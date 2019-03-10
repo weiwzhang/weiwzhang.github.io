dfd025bc8bd6f32c405e14d7db69b96d6cbdcd92
Weiwei Zhang <weiwzhang@berkeley.edu>
2019-03-10 15:57:18 -0700

    finish basic skeleton

--- js/index.js
+++ js/index.js
@@ -1,19 +1,34 @@
-import Typed from 'typed.js';
+window.onload = function() {
+    typeWriter();
+    // $('footer').fadeIn(1000);
+    // $('profile').fadeIn(1000);
+};
+
 
 // add typing effect to name on home page
-var typed = new Typed('name', {
-  strings: ["Weiwei", "蔚蔚"],
-  typeSpeed: 30
-});
+var i = 0;
+var txt = "Hi there, I'm Weiwei!"; /* The text */
+var speed = 60; /* The speed/duration of the effect in milliseconds */
 
-document.getElementById('name').innerHTML = typed;
-
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("home_title").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
 
 // change profile picture on click
 function changeProfileImg() {
-	if (document.getElementById("profile_img").src == "./img/caricature.jpg") {
-        document.getElementById("profile_img").src == "./img/linkedin_pic.jpg"
+	if (document.getElementById("profile_img").src.endsWith("img/caricature.jpg")) {
+        document.getElementById("profile_img").src = "./img/linkedin_pic.jpg";
+        document.getElementById("profile_img").style = "width: 21vw; height: 28vh"
     } else {
-    	document.getElementById("profile_img").src == "./img/caricature.jpg"
+    	document.getElementById("profile_img").src = "./img/caricature.jpg";
+        document.getElementById("profile_img").style = "width: 14vw; height: 28vh"
     }
 }
+
+
+
+// https://github.com/lmgonzalves/brushstroke
